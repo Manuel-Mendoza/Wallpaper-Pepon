cfccc9b41a544cfae80f524810b11a49c5f17b3e
Manuel Mendoza <Joseguns2@gmail.com>
2022-03-31 12:14:30 -0500

    Update 1era parte

---
+++ src/Date.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from "react";
+
+export default function () {
+  let date = new Date();
+  const [hora, setHora] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [segundo, setSegundo] = useState(0);
+  useEffect(() => {
+    console.log("as");
+    setInterval(() => {
+      let date = new Date();
+      setHora(date.getHours());
+      setMinutes(date.getMinutes());
+      setSegundo(date.getSeconds());
+    }, 1000);
+  }, []);
+  return (
+    <div
+      className={`App ${
+        hora === 6
+          ? "dia"
+          : hora === 12
+          ? "mediodia"
+          : hora === 4
+          ? "tarde"
+          : hora === 6
+          ? "tarde_noche"
+          : "noche"
+      }`}
+    >
+      <h1>{date.toDateString()}</h1>
+      <h2>{`${hora}h: ${minutes}m  ${segundo}s`}</h2>
+    </div>
+  );
+}
