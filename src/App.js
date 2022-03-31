a9524591a46edd39dcc3fbc91f0b6785c82a66e0
Manuel Mendoza <Joseguns2@gmail.com>
2022-03-31 10:25:01 -0500

    Inicio

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+export default function () {
+  let date = new Date();
+  const [hora, setHora] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [segundo, setSegundo] = useState(0);
 
-function App() {
+  useEffect(() => {
+    console.log("as");
+    setInterval(() => {
+      let date = new Date();
+      setHora(date.getHours());
+      setMinutes(date.getMinutes());
+      setSegundo(date.getSeconds());
+    }, 1000);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="App"
+      style={{
+        background: minutes === 4 ? "#000" : "#159",
+        transition: "all 2s",
+      }}
+    >
+      <h1>{date.toDateString()}</h1>
+      <h2>{`${hora}h: ${minutes}m  ${segundo}s`}</h2>
     </div>
   );
 }
-
-export default App;
